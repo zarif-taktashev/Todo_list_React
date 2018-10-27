efdc0369e027ceebccded73a4daf09a5960f97ee
zarif-taktashev <eldarado_1967@mail.ru>
2018-10-27 16:41:49 +0600

    todo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var TodoItem = React.createClass({
+	delete: function(){
+		this.props.delete(this.props.todo);
+	},
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	render: function(){
+		return <li>{this.props.todo}<span onClick={this.delete} className="delete">Delete</span></li>
+	}
+}); 
+
+var TodoList = React.createClass({
+	getInitialState: function(){
+		var initialList = (typeof localStorage["todos"] != "undefined" ? JSON.parse(localStorage.getItem('todos')) : ['Create todo list', 'Read a book', 'Create blog', 'learn jest']);
+		
+		return{
+			list:initialList,
+			inputValue: ''
+		};
+	},
+	updateInputValue: function(evt){
+		this.setState({
+			inputValue: evt.target.value
+		});
+	},
+	handleAdd: function(){
+		var todos = this.state.list;
+		var newInput = this.state.inputValue;
+		todos.push(newInput);
+		localStorage.setItem('todos', JSON.stringify(todos));
+		this.setState({
+			list: todos,
+			inputValue: ''
+		});
+	},
+	delete: function(todo){
+		var todos = this.state.list;
+		todos.splice(todos.indexOf(todo), 1);
+		localStorage.setItem('todos', JSON.stringify(todos));
+		this.setState({
+			list: todos
+		});
+	},
+	render: function(){
+		var listItems = this.state.list.map(function(listValue){
+			return <TodoItem todo={listValue} delete={this.delete} />
+		}.bind(this));
+
+		return(
+			<div>
+				<h1>My Todo List</h1>
+				<ul>{listItems}</ul>
+				<div className="inputBar">
+					<input type="text" value={this.state.inputValue} onChange={this.updateInputValue} />
+					<button onClick={this.handleAdd}>Add</button>
+				</div>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
